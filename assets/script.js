13cbb7f1362ef3664c99e591ce5177d4e7cbd3d6
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-06-30 14:30:57 -0400

    working-calender+clock!

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,60 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+//$(function () {
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
+    // local storage. 
+    
+    //HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-    //
+    
+
+
+
     // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
+    // block by comparing the id to the current hour. 
+    
+    //HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
+    
+//$(function ()
+//{
+//    upToTime(displayClock(), 1000);
+//})
+function displayClock(){
+    var todaysDate = dayjs().format('MMM D, YYYY');
+$('#todaysDate').text(todaysDate);
+
+var weekDay = dayjs().format('dddd');
+$('#currentDay').text(weekDay);
+
+var time = dayjs().format('hh:mm:ss');
+$('#currentTime').text(time);
+
+
+}
+ setInterval (displayClock, 1000);
+          //here I will attempt to add the past class to 
+          var past = $(".description");
+          // have a 
+          past.addClass = (".past");
+
+        var present = $(".description");
+        present.addClass = (".present");
+
+        var future = $(".description");
+        future.addClass = (".present");
+
+
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+ // });
